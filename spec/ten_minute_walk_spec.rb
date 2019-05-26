73dc3d8db72b3c69411855f80e30510ba06ab637
dangroze <dangroze@gmail.com>
2019-05-26 19:13:24 +0100

    :white_check_mark:Test-arrive at starting point

--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -3,11 +3,15 @@ require 'ten_minute_walk'
 RSpec.describe 'is_valid?' do
 
   it 'returns false when walk is less than 10 minutes' do
-    expect(is_valid(['w','e','s','n'])).to eq(false)
+    expect(is_valid?(['w','e','s','n'])).to eq(false)
   end
 
   it 'returns false when walk is more than 10 minutes' do
-    expect(is_valid(['w','e','s','n','w','e','s','n''w','e','s','n'])).to eq(false)
+    expect(is_valid?(['w','e','s','n','w','e','s','n''w','e','s','n'])).to eq(false)
   end
-  
+
+  it 'returns true when you come back to starting point' do
+    expect(is_valid?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
+  end
+
 end
