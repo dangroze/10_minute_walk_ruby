7f209d3859d9c1d20c680a38d91f5d4d7ad2a5c9
dangroze <dangroze@gmail.com>
2019-05-26 19:08:26 +0100

    :white_check_mark:Passed test-walk more than 10m

--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -1,9 +1,13 @@
 require 'ten_minute_walk'
 
-RSpec.describe '#is_valid?' do
+RSpec.describe 'is_valid?' do
 
   it 'returns false when walk is less than 10 minutes' do
-    expect(is_valid?(['w','e','s','n'])).to eq(false)
+    expect(is_valid(['w','e','s','n'])).to eq(false)
+  end
+
+  it 'returns false when walk is more than 10 minutes' do
+    expect(is_valid(['w','e','s','n','w','e','s','n''w','e','s','n'])).to eq(false)
   end
   
 end
